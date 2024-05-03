bbf4f8ead45d85f549aa861e5f6928ded9962b8e
Gabriel Talbert Bunt <gabriel@gabrieltb.me>
2024-05-03 00:41:21 -0400

    add extremely scuffed multithreading

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use image::io::Reader as ImageReader;
 use image::ImageBuffer;
 use image::Luma;
+use rayon::prelude::*;
 
 fn main() {
     // let img = ImageReader::open("disp2.pgm").unwrap().decode().unwrap();
@@ -21,60 +22,73 @@ fn main() {
     println!("{disparities:#?}");
 
     let padding: i64 = 1;
-    for y in 0..left.height() {
-        println!("{y}");
-        for left_x in 0..left.width() {
-            let mut best_sad = u32::MAX;
-            let mut best_x = 0;
-            for right_x in 0..right.width() {
-                let mut sad: u32 = 0;
-                for offset_x in -padding..=padding {
-                    for offset_y in -padding..=padding {
-                        let final_left_x = left_x as i64 + offset_x;
-                        let final_right_x = right_x as i64 + offset_x;
-                        let final_y = y as i64 + offset_y;
 
-                        let left_pixel;
-                        let right_pixel;
+    let thread_results: Vec<(Vec<u32>, Vec<u32>)> = (0..left.height())
+        .into_par_iter()
+        .map(|y| {
+            let mut disparities_thread = vec![];
+            let mut correspondences_thread = vec![];
+            println!("{y}");
+            for left_x in 0..left.width() {
+                let mut best_sad = u32::MAX;
+                let mut best_x = 449;
+                for right_x in 0..right.width() {
+                    let mut sad: u32 = 0;
+                    for offset_x in -padding..=padding {
+                        for offset_y in -padding..=padding {
+                            let final_left_x = left_x as i64 + offset_x;
+                            let final_right_x = right_x as i64 + offset_x;
+                            let final_y = y as i64 + offset_y;
 
-                        if final_left_x < 0 || final_left_x >= left.width() as i64 {
-                            left_pixel = 0;
-                        } else if final_y < 0 || final_y >= left.height() as i64 {
-                            left_pixel = 0;
-                        } else {
-                            left_pixel = left.get_pixel(final_left_x as u32, final_y as u32).0[0];
+                            let left_pixel;
+                            let right_pixel;
+
+                            if final_left_x < 0 || final_left_x >= left.width() as i64 {
+                                left_pixel = 0;
+                            } else if final_y < 0 || final_y >= left.height() as i64 {
+                                left_pixel = 0;
+                            } else {
+                                left_pixel =
+                                    left.get_pixel(final_left_x as u32, final_y as u32).0[0];
+                            }
+
+                            if final_right_x < 0 || final_right_x >= right.width() as i64 {
+                                right_pixel = 0;
+                            } else if final_y < 0 || final_y >= right.height() as i64 {
+                                right_pixel = 0;
+                            } else {
+                                right_pixel =
+                                    right.get_pixel(final_right_x as u32, final_y as u32).0[0];
+                            }
+
+                            let ad = if left_pixel > right_pixel {
+                                left_pixel - right_pixel
+                            } else {
+                                right_pixel - left_pixel
+                            };
+
+                            sad += ad as u32;
                         }
-
-                        if final_right_x < 0 || final_right_x >= right.width() as i64 {
-                            right_pixel = 0;
-                        } else if final_y < 0 || final_y >= right.height() as i64 {
-                            right_pixel = 0;
-                        } else {
-                            right_pixel =
-                                right.get_pixel(final_right_x as u32, final_y as u32).0[0];
-                        }
-
-                        let ad = if left_pixel > right_pixel {
-                            left_pixel - right_pixel
-                        } else {
-                            right_pixel - left_pixel
-                        };
-
-                        sad += ad as u32;
+                    }
+                    if sad < best_sad {
+                        best_sad = sad;
+                        best_x = right_x;
                     }
                 }
-                if sad < best_sad {
-                    best_sad = sad;
-                    best_x = right_x;
-                }
+                correspondences_thread.push(best_x);
+                disparities_thread.push(if best_x > left_x {
+                    best_x - left_x
+                } else {
+                    left_x - best_x
+                });
             }
-            correspondences.push(best_x);
-            disparities.push(if best_x > left_x {
-                best_x - left_x
-            } else {
-                left_x - best_x
-            });
-        }
+            (correspondences_thread, disparities_thread)
+        }) // What is this formatting? lol
+        .collect();
+
+    for (correspondences_thread, disparities_thread) in thread_results {
+        correspondences.extend(correspondences_thread);
+        disparities.extend(disparities_thread);
     }
 
     println!("{disparities:#?}");
